ae0d59a3d664341e88a365ebcea3faa64a3ec4fa
Russell Strauss <russellstrauss@gmail.com>
2019-10-14 16:20:05 -0400

    Add beats file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 var Scene = require('./components/scene.js');
 var UI = require('./components/ui.js');
 var Utilities = require('./utils.js');
+var Beats = require('./beats.js');
 var Graphics = require('./graphics.js');
 
 (function () {
